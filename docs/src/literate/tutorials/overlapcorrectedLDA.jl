b9b86daba4438f017243be718443097386960390
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-05-10 12:22:30 +0000

    unfold 0.7

--- docs/src/literate/tutorials/overlapcorrectedLDA.jl
+++ docs/src/literate/tutorials/overlapcorrectedLDA.jl
@@ -10,23 +10,23 @@ using Unfold
 # ## Simulation
 # We start with simulating some continuous, overlapping data with two conditions. 
 # As of now, `UnfoldSim` doesnt support multichannel, so we simply repeat the channel and add some noise`
-dat,evt = UnfoldSim.predef_eeg()
-dat = repeat(dat',5)
+dat, evt = UnfoldSim.predef_eeg()
+dat = repeat(dat', 5)
 dat .= dat .+ 20 .* rand(size(dat)...)
 
 # ## Overlap-model Definition
 # We have to define what model we want to use for overlap correction
 # We decide for a one-basisfunction model, with one covariate, from `-0.1` to `0.5` seconds afte the stimulus onset. Sampling rate `100` as in the simulation
-des = Dict(Any => (@formula(0~1+condition+continuous),firbasis((-0.1,1.),100)));
+des = [Any => (@formula(0 ~ 1 + condition + continuous), firbasis((-0.1, 1.0), 100))];
 # Fitting and visualizing a single channel of the model
-uf = Unfold.fit(UnfoldModel,des,evt,dat[1,:]);
+uf = Unfold.fit(UnfoldModel, des, evt, dat[1, :]);
 plot_erp(coeftable(uf))
 
 # ## Fitting the Overlap-corrected LDA model
 # Following the [MLJ Modelzoo](https://alan-turing-institute.github.io/MLJ.jl/dev/list_of_supported_models/) we have to do the following to load an LDA model
 using MLJ, MultivariateStats, MLJMultivariateStatsInterface
-LDA = @load LDA pkg=MultivariateStats
-uf_lda = fit(UnfoldDecodingModel,des,evt,dat,LDA(),Any=>:condition;nfolds=2) # 2 folds to speed up computation
+LDA = @load LDA pkg = MultivariateStats
+uf_lda = fit(UnfoldDecodingModel, des, evt, dat, LDA(), Any => :condition; nfolds=2) # 2 folds to speed up computation
 plot_erp(coeftable(uf_lda))
 
 # Voila, the model classified the correct period. 
--- src/overlap_corrected.jl
+++ src/overlap_corrected.jl
@@ -1,77 +1,27 @@
 """
-Returns the single trial corrected (and "epoched") trials from the basis `target`
+Returns the single trial corrected (and "epoched") trials from the basis `target_eventname`
 
 """
-function singletrials(dat,uf_train::UnfoldLinearModelContinuousTime,
-	tbltrain::DataFrame,target,eventcolumn)
-  
-	basis = Unfold.design(uf_train)[target][2]
-		
-		
-	eventTbl = subsetEvents(tbltrain,target,eventcolumn)
+function singletrials(dat, uf_train::UnfoldLinearModelContinuousTime,
+    tbltrain::AbstractDataFrame, target_eventname, eventcolumn)
 
-	
-	onsets = eventTbl.latency
-	startIX = floor.(onsets) .+ Unfold.shiftOnset(basis)
-	stopIX = startIX .+ length(Unfold.times(basis)).-1
-	
-	
-	X_train = modelmatrix(uf_train)
 
-	# get predicted singler trial events
-	# XXX multi-basis, this will select the wrong formula
+    #pred_notarget = Unfold.predict_partial_overlap(uf_train, coef(uf_train), [tbltrain]; exclude_basis=[target_eventname], epoch_to=target_eventname, eventcolumn)
+    basisnames = Unfold.basisname(uf_train)
+    ix = findfirst(target_eventname .== basisnames)
+    pred = Unfold.predict(uf_train, overlap=false)[ix]
+    tw = Unfold.calc_epoch_timewindow(uf_train, target_eventname)
 
-	#@assert(target == Any, "multibasis not yet implemented, probably here the only problem could occur!")
-	forms =  Unfold.formula(uf_train)
-	if forms isa Array
-		# find out which basis to use for decoding
-		function whichevent(uf,target,eventcolumn)
-			for (ix,e) = enumerate(Unfold.designmatrix(uf).events)
-				if e[1,eventcolumn] == target
-					return ix
-				end
-			end
-		end
-		ix = whichevent(uf_train,target,eventcolumn)
-		
-	else
-		forms = [forms.rhs]
-		ix = 1
-	end
-	
-	fromTo,timesvec,eff =  Unfold.yhat(uf_train,forms,eventTbl)
-	if forms isa Array
-		
-		mxsum = cumsum(maximum.(fromTo))
-		
-		
-		fromTo = fromTo[ix] 
-		if ix>1
-			fromTo .+= mxsum[ix-1]
-		end
-	else
-		fromTo = fromTo[1]
-	end
 
-	
-	out_e = Array{Float64}(undef,size(dat,1),fromTo.step,length(fromTo))
-	perevent_views = view.(Ref(eff),fromTo,Ref(1:size(eff,2)))
+    find_lat =
+        x -> x[
+            target_eventname == Any ? (1:size(x, 1)) : x[:, eventcolumn] .== target_eventname,
+            :latency,
+        ]
+    latencies = vcat(find_lat(tbltrain))
 
-	for e = 1:length(onsets)
-	
-		# index
-		ix = startIX[e]:(stopIX[e]-1)
-		# clip index, necessary for out_e
-		ixix = (ix.>0) .&& (ix .< size(dat,2))
-	
-		# index into data / designmatrix
-		dataIx = ix[ixix]
-		# calculate residuals
-		resid =   @view(dat[:,dataIx]) .- (@view(X_train[dataIx,:]) * coef(uf_train)')'
-		# add back the current response, now overlap corrected :)
-		ix_eff = (fromTo[e]:fromTo[e]+fromTo.step-1)
-		
-		out_e[:,ixix,e] .=resid  .+ @view(eff[ix_eff[ixix],:])'
-	end
-	return out_e
+    resid_targetonly = Unfold._residuals(pred, dat, latencies, (tw[1], tw[end]))
+
+    return pred, dat, latencies, (tw[1], tw[end])
+    return resid_targetonly
 end
